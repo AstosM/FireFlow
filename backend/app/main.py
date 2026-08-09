962a261fea9712bc2f79ddd0801fa77883ea39a2
AstosM <astosm123@gmail.com>
2026-08-10 01:55:05 +0530

    add commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
 
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "https://fireflow-frontend.onrender.com"
 
         # Add your Vercel URL here after deployment
         # "https://your-fireflow.vercel.app",
